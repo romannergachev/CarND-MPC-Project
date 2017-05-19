f9beb93ff851d55942a5099608915822ace4539c
Roman Nergachev <nergachev.roman@gmail.com>
2017-05-19 18:08:48 +0300

    feat: Implement MPC

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -5,8 +5,21 @@
 
 using CppAD::AD;
 
-size_t N = 25;
-double dt = 0.05;
+/*
+ * Intuitively it makes sense for N to be as big as possible, since that will result in more simulated actuations.
+ * But is it really worth it to perform all these simulated actuations if we only end up making use of the first one?
+ * Recall we only actually use the actuation commands at the first timestep due to imperfections in the model!
+ * After a certain point simulating more actuations doesn't influence the initial actuation commands.
+ * We also have to be mindful of the computational cost. We'll likely have a deadline when an actuation
+ * has to be performed. Thus, there's no correct answer for the value of N; it's application dependent.
+ * */
+size_t N = 10;
+/* Again, a large value of dt might seem appropriate. After all, if N is 25 and dt is 1 we're making predictions
+ * for 24 seconds! The caveat is in those 24 seconds, only 24 actuations are predicted. For a 24 second span in
+ * a realtime environment that's insufficient. Instead, if dt is 0.1, 24 actuations in are predicted in 2.4 seconds.
+ * A good rule of thumb is to set dt based on how often we expect changes to occur in the environment.
+ * */
+double dt = 0.15;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -21,10 +34,10 @@ double dt = 0.05;
 const double Lf = 2.67;
 
 // Both the reference cross track and orientation errors are 0.
-// The reference velocity is set to 40 mph.
+// The reference velocity is set to 80 mph.
 double ref_cte = 0;
 double ref_epsi = 0;
-double ref_v = 40;
+double ref_v = 80;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -52,21 +65,21 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (int i = 0; i < N; i++) {
-      fg[0] += CppAD::pow(vars[cte_start + i] - ref_cte, 2);
-      fg[0] += CppAD::pow(vars[epsi_start + i] - ref_epsi, 2);
+      fg[0] += 1400 * CppAD::pow(vars[cte_start + i] - ref_cte, 2);
+      fg[0] += 1400 * CppAD::pow(vars[epsi_start + i] - ref_epsi, 2);
       fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (int i = 0; i < N - 1; i++) {
-      fg[0] += CppAD::pow(vars[delta_start + i], 2);
-      fg[0] += CppAD::pow(vars[a_start + i], 2);
+      fg[0] += 30 * CppAD::pow(vars[delta_start + i], 2);
+      fg[0] += 60 * CppAD::pow(vars[a_start + i], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (int i = 0; i < N - 2; i++) {
-      fg[0] += CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-      fg[0] += CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+      fg[0] += 290 * CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+      fg[0] += 130 * CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
     }
 
     //
@@ -108,27 +121,15 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + i];
       AD<double> a0 = vars[a_start + i];
 
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0;
-      AD<double> psides0 = CppAD::atan(coeffs[1]);
+      AD<double> f0      = coeffs[3] * x0 * x0 * x0 + coeffs[2] * x0 * x0 + coeffs[1] * x0 + coeffs[0];
+      AD<double> psides0 = CppAD::atan((3 * coeffs[3] * (x0 * x0)) + (2 * coeffs[2] * x0) + coeffs[1]);
 
-      // Here's `x` to get you started.
-      // The idea here is to constraint this value to be 0.
-      //
-      // Recall the equations for the model:
-      // x_[t+1] = x[t] + v[t] * cos(psi[t]) * dt
-      // y_[t+1] = y[t] + v[t] * sin(psi[t]) * dt
-      // psi_[t+1] = psi[t] + v[t] / Lf * delta[t] * dt
-      // v_[t+1] = v[t] + a[t] * dt
-      // cte[t+1] = f(x[t]) - y[t] + v[t] * sin(epsi[t]) * dt
-      // epsi[t+1] = psi[t] - psides[t] + v[t] * delta[t] / Lf * dt
       fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
       fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
       fg[2 + psi_start + i] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
       fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
-      fg[2 + cte_start + i] =
-        cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
-      fg[2 + epsi_start + i] =
-        epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+      fg[2 + cte_start + i] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+      fg[2 + epsi_start + i] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
     }
   }
 };
@@ -139,7 +140,7 @@ class FG_eval {
 MPC::MPC() {}
 MPC::~MPC() {}
 
-vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
+vector<vector<double>> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   bool ok = true;
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
@@ -187,8 +188,8 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // degrees (values in radians).
   // NOTE: Feel free to change this to something else.
   for (int i = delta_start; i < a_start; i++) {
-    vars_lowerbound[i] = -0.436332;
-    vars_upperbound[i] = 0.436332;
+    vars_lowerbound[i] = -0.55;
+    vars_upperbound[i] = 0.55;
   }
 
   // Acceleration/decceleration upper and lower limits.
@@ -254,12 +255,11 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
 
-  // Cost
-  auto cost = solution.obj_value;
-  std::cout << "Cost " << cost << std::endl;
+  vector<double> x_values(&solution.x[x_start + 1],&solution.x[x_start + N - 2]);
+  vector<double> y_values(&solution.x[y_start + 1],&solution.x[y_start + N - 2]);
 
-  return {solution.x[x_start + 1],   solution.x[y_start + 1],
-          solution.x[psi_start + 1], solution.x[v_start + 1],
-          solution.x[cte_start + 1], solution.x[epsi_start + 1],
-          solution.x[delta_start],   solution.x[a_start]};
+  vector<double> delta_value = {solution.x[delta_start]};
+  vector<double> a_value     = {solution.x[a_start]};
+
+  return {x_values, y_values, delta_value, a_value};
 }
